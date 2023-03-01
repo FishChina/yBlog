6e731b90988c1151f881b2235cf71606901ae490
yudehai <yudehai@cn.ibm.com>
2023-03-01 16:01:16 +0800

    update java

--- blogapi/src/main/java/com/ydh/blogapi/po/Blog.java
+++ blogapi/src/main/java/com/ydh/blogapi/po/Blog.java
@@ -1,17 +1,27 @@
-package com.ydh.blogapi.po;
+package com.star.po;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @Author: ONESTAR
+ * @QQ群: 530311074
+ * @URL: https://onestar.newstar.net.cn/
+ */
 @Entity
 @Table(name = "t_blog")
-public class blog {
+public class Blog {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
+    @Basic(fetch = FetchType.LAZY)
+    @Lob
     private String content;
     private String firstPicture;
     private String flag;
@@ -21,10 +31,29 @@ public class blog {
     private boolean commentabled;
     private boolean published;
     private boolean recommend;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createTime;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date updateTime;
 
-    public blog() {
+    @ManyToOne
+    private Type type;
+
+//    @ManyToMany(cascade = {CascadeType.PERSIST})
+//    private List<Tag> tags = new ArrayList<>();
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "blog")
+    private List<Comment> comments = new ArrayList<>();
+
+    @Transient
+    private String tagIds;
+
+    private String description;
+
+    public Blog() {
     }
 
     public Long getId() {
@@ -131,13 +160,83 @@ public class blog {
         this.updateTime = updateTime;
     }
 
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+//    public List<Tag> getTags() {
+//        return tags;
+//    }
+
+//    public void setTags(List<Tag> tags) {
+//        this.tags = tags;
+//    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
 
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+
+    public String getTagIds() {
+        return tagIds;
+    }
+
+    public void setTagIds(String tagIds) {
+        this.tagIds = tagIds;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public void init() {
+//        this.tagIds = tagsToIds(this.getTags());
+//    }
+
+    //1,2,3
+//    private String tagsToIds(List<Tag> tags) {
+//        if (!tags.isEmpty()) {
+//            StringBuffer ids = new StringBuffer();
+//            boolean flag = false;
+//            for (Tag tag : tags) {
+//                if (flag) {
+//                    ids.append(",");
+//                } else {
+//                    flag = true;
+//                }
+//                ids.append(tag.getId());
+//            }
+//            return ids.toString();
+//        } else {
+//            return tagIds;
+//        }
+//    }
 
 
     @Override
     public String toString() {
-        return "blog{" +
+        return "Blog{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
@@ -151,6 +250,12 @@ public class blog {
                 ", recommend=" + recommend +
                 ", createTime=" + createTime +
                 ", updateTime=" + updateTime +
+                ", type=" + type +
+//                ", tags=" + tags +
+                ", user=" + user +
+                ", comments=" + comments +
+                ", tagIds='" + tagIds + '\'' +
+                ", description='" + description + '\'' +
                 '}';
     }
 }
